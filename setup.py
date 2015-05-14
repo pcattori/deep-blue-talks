e9164b32ae173159c183ac9f8d931eaf7b2e0b31
Pedro Cattori <pcattori@gmail.com>
2015-05-14 19:17:31 -0400

    PyPI ready

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 from setuptools import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 setup(name='deep-blue-talks',
       version='0.1',
       description='bestow speech unto chess engines',
-      long_description=readme(),
+      long_description=open('README.md').read(),
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
           'Programming Language :: Python :: 2.7',
           'Topic :: Scientific/Engineering :: Artificial Intelligence',
-      ]
+          'Topic :: Games/Entertainment :: Board Games',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       keywords='deep blue talks chess pedro cattori',
       url='https://github.com/pcattori/deep-blue-talks',
       author='Pedro Cattori',
       author_email='pcattori@gmail.com',
       license='MIT',
-      packages=['deep-blue-talks'],
+      packages=['kasparobot'],
       install_requires=[
           'python-chess',
       ],
+      dependency_links=[
+          "git+https://github.com/niklasf/python-chess.git@883a491b414ab9c30a2021241ad4d2af2113c868#egg=python-chess"
+      ],
+      scripts=['bin/deep-blue-talks'],
       include_package_data=True,
       zip_safe=False)
